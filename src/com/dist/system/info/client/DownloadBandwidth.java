08e0f03644fb60bb4f39df6b50edc9ade062b8ee
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-12-02 18:05:41 -0600

    Added speedtest

--- src/com/dist/system/info/client/DownloadBandwidth.java
+++ src/com/dist/system/info/client/DownloadBandwidth.java
@@ -1,13 +1,14 @@
 package com.dist.system.info.client;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DownloadBandwidth {
-    static final String DOWNLOAD_URL = "http://189.152.118.105/pexels-kelly-lacy-5652690.mp4";
+    static final String SPEEDTEST_EXE = "speedtest.exe";
 
     public static void main(String[] args) {
         DownloadBandwidth downloadBandwidth = new DownloadBandwidth();
@@ -16,35 +17,34 @@ public class DownloadBandwidth {
     }
 
     public double calculate() {
-        double speed = 0;
-
         try {
-            URL url = new URL(DOWNLOAD_URL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty(
-                    "User-Agent",
-                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-            InputStream is = connection.getInputStream();
+            String pwd = System.getProperty("user.dir");
+            Path path = Paths.get(pwd, SPEEDTEST_EXE);
 
-            byte [] buf = new byte[2048];
-            int off = -1;
-            int total = 0;
-            long startTime = System.nanoTime();
-            while((off = is.read(buf)) > 0) {
-                total += off;
-            }
-            long endTime = System.nanoTime();
+            Runtime runtime = Runtime.getRuntime();
+            String [] command = {
+                    path.toAbsolutePath().toString(),
+                    "--format=json"
+            };
 
-            double MB = total / 1e6;
-            double duration = (endTime - startTime) / 1e9; // Seconds
+            Process proc = runtime.exec(command);
+            BufferedReader stdInput = new BufferedReader(
+                    new InputStreamReader(proc.getInputStream()));
 
-            if(duration > 0) speed = MB / duration;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            StringBuilder json = new StringBuilder();
+
+            String part = null;
+            while((part = stdInput.readLine()) != null)
+                json.append(part);
+
+            JSONObject jsonObject = new JSONObject(json.toString());
+
+            long maxSpeed = jsonObject.getJSONObject("download").getLong("bandwidth");
+            return ((double) maxSpeed) / 1e6;
+        } catch(Exception e) {
             e.printStackTrace();
         }
 
-        return speed;
+        return 0;
     }
 }
